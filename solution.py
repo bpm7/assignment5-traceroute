9a4111e76a183aa5683ac6a5266347b99ecbd89c
Brian Murphy <98722378+bpm7@users.noreply.github.com>
2022-04-15 15:21:27 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -45,6 +45,16 @@ def build_packet():
 
     # Make the header in a similar way to the ping exercise.
     # Append checksum to the header.
+    ID = os.getpid() & 0xFFFF
+    myCheckSum=0
+    header=struct.pack("bbHHH",ICMP_ECHO_REQUEST,0,myCheckSum,ID,1)
+    data=struct.pack("d",time.time())
+    myCheckSum=checksum(header+data)
+    if sys.platform=='darwin':
+        myCheckSum=htons(myCheckSum) & 0xffff
+    else:
+        myCheckSum= htons(myCheckSum)
+    header=struct.pack("bbHHH",ICMP_ECHO_REQUEST,0,myCheckSum,ID,1)
 
     # Don’t send the packet yet , just return the final packet in this function.
     #Fill in end
@@ -64,12 +74,15 @@ def get_route(hostname):
             destAddr = gethostbyname(hostname)
 
             #Fill in start
+            icmp = getprotobyname("icmp")
             # Make a raw socket named mySocket
+            mySocket=socket(AF_INET, SOCK_RAW, icmp)
             #Fill in end
 
             mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
             mySocket.settimeout(TIMEOUT)
             try:
+                
                 d = build_packet()
                 mySocket.sendto(d, (hostname, 0))
                 t= time.time()
@@ -79,6 +92,7 @@ def get_route(hostname):
                 if whatReady[0] == []: # Timeout
                     tracelist1.append("* * * Request timed out.")
                     #Fill in start
+                    tracelist2.append(tracelist1)
                     #You should add the list above to your all traces list
                     #Fill in end
                 recvPacket, addr = mySocket.recvfrom(1024)
@@ -86,6 +100,7 @@ def get_route(hostname):
                 timeLeft = timeLeft - howLongInSelect
                 if timeLeft <= 0:
                     tracelist1.append("* * * Request timed out.")
+                    tracelist2.append(tracelist1)
                     #Fill in start
                     #You should add the list above to your all traces list
                     #Fill in end
@@ -94,13 +109,19 @@ def get_route(hostname):
 
             else:
                 #Fill in start
+                icmpHeader=recvPacket(1024)
+                ty, code, checksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
+                types=ty
                 #Fetch the icmp type from the IP packet
                 #Fill in end
                 try: #try to fetch the hostname
                     #Fill in start
+                    dest = gethostbyname(hostname)
                     #Fill in end
                 except herror:   #if the host does not provide a hostname
                     #Fill in start
+                    tracelist1.append("Host Not Found")
+                    tracelist2.append(tracelist1)
                     #Fill in end
 
                 if types == 11:
@@ -131,4 +152,5 @@ def get_route(hostname):
                 mySocket.close()
 
 if __name__ == '__main__':
-    get_route("google.co.il")
+    #get_route("google.co.il")
+    get_route("127.0.0.1")
